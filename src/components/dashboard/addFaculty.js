b696d8e90176848328114f5854f393d01e8dcfe0
uncannyRishabh <rishabhrajgupta2000@gmail.com>
2022-06-16 22:39:06 +0530

    - axios

--- src/components/dashboard/addFaculty.js
+++ src/components/dashboard/addFaculty.js
@@ -1,9 +1,83 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, { Component } from 'react'
+import FacultyService from '../../FacultyService'
 import { Container, Field, Heading, FormContainer } from './DashboardComponents'
 
-const AddFaculty = ({ isLoggedIn }) => {
-	if (isLoggedIn)
+class AddFaculty extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            // step 2
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            emailId: ''
+        }
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.saveOrUpdateEmployee = this.saveOrUpdateEmployee.bind(this);
+    }
+
+    // step 3
+    componentDidMount(){
+
+        // step 4
+        if(this.state.id === '_add'){
+            return
+        }else{
+            FacultyService.getFacultyById(this.state.id).then( (res) =>{
+                let employee = res.data;
+                this.setState({firstName: employee.firstName,
+                    lastName: employee.lastName,
+                    emailId : employee.emailId
+                });
+            });
+        }        
+    }
+    saveOrUpdateEmployee = (e) => {
+        e.preventDefault();
+        let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId};
+        console.log('employee => ' + JSON.stringify(employee));
+
+        // step 5
+        if(this.state.id === '_add'){
+            FacultyService.createEmployee(employee).then(res =>{
+                this.props.history.push('/employees');
+            });
+        }else{
+            FacultyService.updateFaculty(employee, this.state.id).then( res => {
+                this.props.history.push('/employees');
+            });
+        }
+    }
+    
+    changeFirstNameHandler= (event) => {
+        this.setState({firstName: event.target.value});
+    }
+
+    changeLastNameHandler= (event) => {
+        this.setState({lastName: event.target.value});
+    }
+
+    changeEmailHandler= (event) => {
+        this.setState({emailId: event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    getTitle(){
+        if(this.state.id === '_add'){
+            return <h3 className="text-center">Add Employee</h3>
+        }else{
+            return <h3 className="text-center">Update Employee</h3>
+        }
+    }
+
+	render(){
+		if (true) //isLoggedIn
 		return (
 			<Container>
 				<Heading>
@@ -73,6 +147,7 @@ const AddFaculty = ({ isLoggedIn }) => {
 							fullWidth
 							variant="contained"
 							sx={{ mt: 3, mb: 2 , height: '54px'}}
+							onClick={this.saveOrUpdateEmployee()}
 						>
 							ADD FACULTY
 						</Button>
@@ -87,6 +162,8 @@ const AddFaculty = ({ isLoggedIn }) => {
 		return (
 			<Container>YOU NEED TO LOGIN OR SIGNUP TO ACCESS</Container>
 		)
+	}
+	
 }
 
 export default AddFaculty
